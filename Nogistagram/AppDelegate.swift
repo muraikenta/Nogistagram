8c5884e970a52676a6dfe4087d2ea8503af55db8
村井謙太 <muraikenta0507@gmail.com>
2016-10-06 20:16:07 +0900

    Facebookログインのクライアントできた

--- Nogistagram/AppDelegate.swift
+++ Nogistagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -40,7 +41,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
 
 }
 
