a33dd88db998cc31cdc537574c1f6101871a79fd
shohohoh <so10ve.q0fsu0u@gmail.com>
2016-10-10 15:30:41 +0900

    emailのvalidation

--- Nogistagram/ViewModels/RegistrationViewModel.swift
+++ Nogistagram/ViewModels/RegistrationViewModel.swift
@@ -15,15 +15,19 @@ class RegistrationViewModel {
     let enableGoToNextButton: Observable<Bool>
     
     init() {
-        // 3文字以上だったらボタンをenbaleにする
-        
-        enableGoToNextButton = Observable.combineLatest(email.asObservable(), email.asObservable()) {
-            $0.0.characters.count >= 3 && $0.1.characters.count >= 3
+        enableGoToNextButton = email.asObservable().map{
+            $0.characters.count > 0
         }
     }
-    
+
     func goToNext() {
         print("email=", email.value)
         email.value = ""
+    }
+
+    // TODO: こんなの使って次へボタンが押されたときにエラーメッセージ出す
+    func isValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        return Regexp(emailRegEx).isMatch(input: email)
     }
 }
