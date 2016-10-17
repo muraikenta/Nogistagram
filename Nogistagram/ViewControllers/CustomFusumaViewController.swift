3426315830ea4ed5d9bc63fdbccad4b7fb7ea760
村井謙太 <muraikenta0507@gmail.com>
2016-10-17 19:39:01 +0900

    remove Fusuma from Cartfile

---
+++ Nogistagram/ViewControllers/CustomFusumaViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CustomeFusumaViewController.swift
+//  Nogistagram
+//
+//  Created by 村井謙太 on 2016/10/17.
+//  Copyright © 2016年 村井謙太. All rights reserved.
+//
+
+import UIKit
+
+class CustomFusumaViewController: FusumaViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction override func doneButtonPressed(_ sender: UIButton) {
+        print("hoge@@@@@@@@@")
+    }
+}
