56a456be06f0baae3d854cdd5276fe912a60c58e
村井謙太 <muraikenta0507@gmail.com>
2016-10-18 23:01:46 +0900

    add Kingfisher

---
+++ Nogistagram/Models/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Nogistagram
+//
+//  Created by 村井謙太 on 2016/10/18.
+//  Copyright © 2016年 村井謙太. All rights reserved.
+//
+
+import Foundation
---
+++ Nogistagram/Views/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Nogistagram
+//
+//  Created by 村井謙太 on 2016/10/18.
+//  Copyright © 2016年 村井謙太. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
