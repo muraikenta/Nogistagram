5196d12ffd3af3cd391bfd62a755b2c074ddd485
村井謙太 <muraikenta0507@gmail.com>
2016-10-18 23:02:15 +0900

    create Post model

--- Nogistagram/Models/Post.swift
+++ Nogistagram/Models/Post.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import RealmSwift
+import ObjectMapper
+
+class Post: Object, Mappable {
+    
+    dynamic var imageUrl =  ""
+    dynamic var body = ""
+    
+    required convenience init?(map: Map) {
+        self.init()
+        self.mapping(map: map)
+    }
+    
+    func mapping(map: Map) {
+        imageUrl <- map["image_url"]
+        body <- map["body"]
+        
+    }
+}
