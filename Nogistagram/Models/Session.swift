6bdfa9881264abde47a8fc96bde3e5f4bfc4b665
shohohoh <so10ve.q0fsu0u@gmail.com>
2016-10-18 18:17:22 +0900

    fix Session Model

--- Nogistagram/Models/Session.swift
+++ Nogistagram/Models/Session.swift
@@ -11,15 +11,15 @@ import RealmSwift
 import ObjectMapper
 
 class Session: Object, Mappable {
-    dynamic var user_id: Int = 0
+    dynamic var userId: Int = 0
     dynamic var name: String!
-    dynamic var unique_name: String!
+    dynamic var uniqueName: String!
     dynamic var email: String!
     dynamic var introduction: String!
     dynamic var website: String!
     dynamic var gender: Int = 0
-    dynamic var image_url: String!
-    dynamic var privacy_type: Int = 0
+    dynamic var imageUrl: String!
+    dynamic var privacyType: Int = 0
     
 
     required convenience init?(map: Map) {
@@ -28,14 +28,14 @@ class Session: Object, Mappable {
     }
     
     func mapping(map: Map) {
-        user_id <- map["user_id"]
+        userId <- map["user_id"]
         name <- map["name"]
-        unique_name <- map["unique_name"]
+        uniqueName <- map["unique_name"]
         email <- map["email"]
         introduction <- map["introduction"]
         website <- map["website"]
         gender <- map["gender"]
-        image_url <- map["image_url"]
-        privacy_type <- map["privacy_type"]
+        imageUrl <- map["image_url"]
+        privacyType <- map["privacy_type"]
     }
 }
