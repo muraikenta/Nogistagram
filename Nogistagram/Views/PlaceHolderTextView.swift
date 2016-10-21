f78a266223b0b86cfb1bf9986ec1fed8b2ba7fac
shohohoh <so10ve.q0fsu0u@gmail.com>
2016-10-21 21:34:42 +0900

    delete comment

--- Nogistagram/Views/PlaceHolderTextView.swift
+++ Nogistagram/Views/PlaceHolderTextView.swift
@@ -11,22 +11,18 @@ import UIKit
 @IBDesignable
 class PlaceHolderTextView: UITextView {
     
-    // プレースホルダーのラベル
     private var placeHolderLable: UILabel = UILabel()
     
-    // MARK: - ストーリーボードからの呼び出し
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         initialize()
     }
     
-    // MARK: - コード上からの呼び出し
     init(frame: CGRect) {
         super.init(frame: frame, textContainer: nil)
         initialize()
     }
     
-    // MARK: - 初期化メソッド
     func initialize() {
         NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldDidChange(textView:)), name: NSNotification.Name.UITextViewTextDidChange, object: nil)
         
@@ -37,7 +33,6 @@ class PlaceHolderTextView: UITextView {
         self.addSubview(placeHolderLable)
     }
     
-    // MARK: - プレースホルダーのテキスト設定
     @IBInspectable var placeHolder: String = "Place Holder" {
         didSet {
             self.placeHolderLable.text = placeHolder
@@ -45,28 +40,24 @@ class PlaceHolderTextView: UITextView {
         }
     }
     
-    // MARK: - プレースホルダーの色の設定
     @IBInspectable var placeHolderColor: UIColor = UIColor.lightGray {
         didSet {
             self.placeHolderLable.textColor = placeHolderColor
         }
     }
     
-    // MARK: - プレースホルダーの枠線の色の設定
     @IBInspectable var borderColor: UIColor = UIColor.clear {
         didSet {
             self.layer.borderColor = borderColor.cgColor
         }
     }
     
-    // MARK: - プレースホルダーの枠線の色の設定
     @IBInspectable var borderWidth: CGFloat = 0 {
         didSet {
             self.layer.borderWidth = borderWidth
         }
     }
     
-    // MARK: - プレースホルダーの角の丸みの設定
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
@@ -78,7 +69,6 @@ class PlaceHolderTextView: UITextView {
         placeHolderLable.isHidden = (0 == text.characters.count) ? false : true
     }
     
-    // MARK: - 通知の解放
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
